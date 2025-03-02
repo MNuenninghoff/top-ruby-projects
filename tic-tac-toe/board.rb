31761846071501100afaadd318de7d32ac3f915c
MNuenninghoff <mikenue@gmail.com>
2025-03-01 19:57:59 -0600

    wrote initialize and display board methods in board class

--- tic-tac-toe/board.rb
+++ tic-tac-toe/board.rb
@@ -1,3 +1,20 @@
+require_relative 'player.rb'
+require_relative 'board.rb'
+
 class Board
-    
+    def initialize
+      @board = Array.new(3, Array.new(3, "-"))
+      @player1 = Player.new()
+      @palyer2 = Player.new()
+    end
+
+    def display_board
+      put "1 2 3"
+      board.each_with_index do |row, index|
+        print "#{(index + 65).chr} "
+        row.each do |space|
+          print "#{space} "
+        end
+      end
+    end
 end
